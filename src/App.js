2c1145e94b0456f63aef1b860cec2078109a5fec
Toan <votoan410@gmail.com>
2022-10-27 00:22:40 -0500

    minor change before final deployment

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Navbar />
 
           <Routes>
-            <Route path={["/home", "/"]} element={<Home />} />
+            <Route path="/home" element={<Home />} />
             {/* <Route path="/about" element={}/> */}
             <Route path="/projects" element={<Projects />} />
             {/* different routes for projects:  */}
