29066cfb08dbd3fb6f86eb67e99e8d96bed05825
Toan <votoan410@gmail.com>
2022-11-05 23:13:12 -0500

    fix css in navbar, options being hidden issue

--- src/Pages/PersonalGraphs.js
+++ src/Pages/PersonalGraphs.js
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function PersonalGraphs() {
-  return <div className="container mt-5"></div>;
+  return <div className="container mt-5">Graphing</div>;
 }
 
 export default PersonalGraphs;
